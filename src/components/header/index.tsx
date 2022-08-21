b494aa2b6be74871af6ace1f47e18127df304baa
Jay Oh <jungwon.oh0920@gmail.com>
2022-08-20 21:56:15 -0700

    Added react router dom

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function index() {
+    return (
+        <div>
+            <div>About</div>
+            <div>Second Menu</div>
+            <div>Third Menu</div>
+        </div>
+    )
+}
+
+export default index
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>Home Page will come soon... </div>
+    )
+}
+
+export default Home
