e0faede412d233fd7a460c74905d2ce2a7e92017
Jay Oh <jungwon.oh0920@gmail.com>
2022-08-23 22:56:52 -0700

    added a logo and some styling on the header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
+import Logo from '../../assets/tmdb-logo.svg';
 
 function index() {
     return (
-        <ul className='w-full flex bg-teal-200'>
-            <li><NavLink to='movies'>Movies</NavLink></li>
-            <li><NavLink to='tv-shows'>TV Shows</NavLink></li>
-            <li><NavLink to='about'>About</NavLink></li>
-        </ul>
+        <div className='w-full bg-transparent px-4 border-b border-indigo-500'>
+            <div className='max-w-3xl flex item-center mx-auto'>
+                <div className='w-40 flex item-center px-4'>
+                    <img src={Logo} alt='logo' />
+                </div>
+                <ul className='flex place-content-around'>
+                    <li className='p-4'><NavLink to='/'>Movies</NavLink></li>
+                    <li className='p-4'><NavLink to='tv-shows'>TV Shows</NavLink></li>
+                    <li className='p-4'><NavLink to='about'>About</NavLink></li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
