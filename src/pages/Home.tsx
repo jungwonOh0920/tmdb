673c67a7b387a58911c0160b9a78cc6d2aba9083
Jay Oh <jungwon.oh0920@gmail.com>
2022-09-11 21:52:32 -0700

    working on api call

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 function Home() {
+
+    React.useEffect(() => {
+        const api_key = process.env.REACT_APP_TMDB_API_KEY
+        const url = `https://api.themoviedb.org/3/movie/550?api_key=${api_key}`
+
+    })
+
     return (
         <div>Home Page will come soon... </div>
     )
