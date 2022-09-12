bc1508b51af13726a01d4272a5699dab2863dfdf
Jay Oh <jungwon.oh0920@gmail.com>
2022-09-11 21:10:50 -0700

    added layout to app

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,7 @@ import Logo from '../../assets/tmdb-logo.svg';
 function index() {
     return (
         <div className='w-full bg-transparent px-4 border-b border-indigo-500'>
-            <div className='max-w-3xl flex item-center mx-auto'>
+            <div className='max-w-7xl flex item-center mx-auto bg-red-500'>
                 <div className='w-40 flex item-center px-4'>
                     <img src={Logo} alt='logo' />
                 </div>
