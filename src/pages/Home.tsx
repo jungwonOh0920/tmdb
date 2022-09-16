9609adcae0da6c7487c11230ddaeace45e325b3b
Jay Oh <jungwon.oh0920@gmail.com>
2022-09-15 21:52:27 -0700

    called api for images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,20 +2,48 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 function Home() {
-    const [data, setData] = useState()
+    type ResultType = {
+        // adult: boolean,
+        // backdrop_path: string,
+        // genre_ids: [],
+        // id: number,
+        // original_language: string,
+        // original_title: string,
+        // overview: string,
+        // popularity: number,
+        poster_path: string,
+        // release_date: string,
+        // title: string,
+        // video: boolean,
+        // vote_average: number,
+        // vote_count: number
+    }
+    type DataType = {
+        page: number,
+        results: ResultType[],
+        total_pages: number,
+        total_results: number
+    }
+    const [data, setData] = useState<DataType>()
 
     React.useEffect(() => {
         const api_key = process.env.REACT_APP_TMDB_API_KEY
-        const url = `https://api.themoviedb.org/3/movie/550?api_key=${api_key}`
+        const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`
 
         axios.get(url).then(res => {
             console.log(res.data)
-            setData(res.data.title)
+            setData(res.data)
         })
-    })
+    }, [])
 
     return (
-        <div className=''>{data}</div>
+        <div className=''>
+            {
+                data?.results.map((d, idx) => {
+                    return <img src={`https://image.tmdb.org/t/p/original${d.poster_path}`} key={idx} />
+                })
+            }
+        </div>
     )
 }
 
