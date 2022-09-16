f2e5f7b2734f758f0979ab892732fff949e86f86
Jay Oh <jungwon.oh0920@gmail.com>
2022-09-15 21:53:01 -0700

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -31,7 +31,6 @@ function Home() {
         const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`
 
         axios.get(url).then(res => {
-            console.log(res.data)
             setData(res.data)
         })
     }, [])
