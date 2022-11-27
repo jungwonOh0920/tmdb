bb3e826c1ec482b839c830449aa968919dc520dd
Jay Oh <jungwon.oh0920@gmail.com>
2022-11-27 13:21:14 -0800

    created hero that has animation when mobile

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,33 @@
-import Header from "../Header/Header";
+import { useEffect, useState } from "react"
+import eventBus from "../../assets/utilities/EventBus"
+import Header from "../Header/Header"
 import './layout.scss'
 
-const index = ({ children }: any) => {
+const Layout = ({ children }: any) => {
+  const [width, setWidth] = useState<number>(window.innerWidth)
+
+  const handleResize = () => {
+    setWidth(window.innerWidth)
+    eventBus.dispatch('adjustWidth', width)
+  }
+
+  useEffect(() => {
+    handleResize()
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [width])
+
   return (
     <div className="layout">
       <Header />
-      <main className="max-w-7xl w-full mx-auto">
+      <main className="w-full px-20">
         {children}
       </main>
     </div>
   );
 };
 
-export default index;
+export default Layout;
