4142c339355d5d2fc0ae661933f0cbb507db5e3f
Jay Oh <jungwon.oh0920@gmail.com>
2022-12-27 00:02:30 -0800

    authentication on firebase

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className='text-3xl'>
+        <div className='text-3xl text-white'>
             The quick brown fox jumps over the lazy dog
         </div>
     )
--- src/pages/Shows.tsx
+++ src/pages/Shows.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Shows() {
     return (
-        <div className=''>Shows - Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ratione iusto voluptate similique nihil quos mollitia aspernatur animi id possimus sed odio, cupiditate maxime cum voluptas, odit voluptatem voluptatum voluptatibus.</div>
+        <div className='text-white'>Shows - Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ratione iusto voluptate similique nihil quos mollitia aspernatur animi id possimus sed odio, cupiditate maxime cum voluptas, odit voluptatem voluptatum voluptatibus.</div>
     )
 }
 
