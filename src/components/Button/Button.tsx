8f72f812176130c01964fb43a2e0281822815ef5
hyeonbeom.lee <hyeonbeom920723@bitbucket.org>
2023-01-11 22:07:34 -0700

    Button done

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,12 @@
-import { MouseEvent, Component } from "react";
 import { NavLink } from "react-router-dom";
 
 import "./button.scss";
 
 interface Props {
-  // buttonText: string;
-  //   buttonText?: string | null;
   className?: string;
   linkTo?: string;
   children?: JSX.Element | string | null;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  //   onClick?: () => void ;
 }
 
 const Button = ({ children, className, linkTo, onClick }: Props) => {
