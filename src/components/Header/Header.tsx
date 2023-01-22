d1e1bc4ef8470dd99d7a91946234c47fcfc293fa
Jay Oh <jungwon.oh0920@gmail.com>
2023-01-21 18:45:14 -0800

    added avatar images

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,46 @@
-import { NavLink } from "react-router-dom";
-import { useContext } from "react";
-import Logo from "../../assets/tmdb-logo.svg";
-import Button from "../Button/Button";
+import { useContext, useEffect, useState } from "react"
+import { NavLink } from "react-router-dom"
+import Logo from "../../assets/tmdb-logo.svg"
+import Button from "../Button/Button"
 import { Context } from '../Layout/Layout'
+import { collection, query, where, getDocs } from "firebase/firestore"
+import { db } from '../../firebase.config'
 import "./header.scss";
 
 function Header() {
+  const [avatar, setAvatar] = useState('')
+  const [initial, setInitial] = useState('')
   const contextUser: any = useContext(Context)
+
+  useEffect(() => {
+    const getURL = async () => {
+      // Create a reference to the avatars collection
+      const avatarsRef = collection(db, 'avatars')
+
+      // Create q query against the collection.
+      const q = query(avatarsRef, where('userRef', '==', contextUser.uid))
+
+      try {
+        const querySnapshot = await getDocs(q)
+        querySnapshot.forEach((d) => {
+          setAvatar(d.data().avatar)
+        })
+      } catch (error) {
+        console.log('err: ', error)
+      }
+
+    }
+
+    if (contextUser) {
+      getURL()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (avatar == '' && contextUser) {
+      setInitial(contextUser.displayName[0])
+    }
+  }, [avatar])
 
   return (
     <div className="header-container">
@@ -15,24 +49,28 @@ function Header() {
           <div className="w-40 px-4">
             <img src={Logo} alt="logo" />
           </div>
-          <ul className="flex place-content-around">
-            <li className="p-4">
-              <NavLink to="/">Movies</NavLink>
-            </li>
-            <li className="p-4">
-              <NavLink to="tv-shows">TV Shows</NavLink>
-            </li>
-            <li className="p-4">
-              <NavLink to="about">About</NavLink>
-            </li>
+          <ul className="header-list">
+            <li><NavLink to="/">Movies</NavLink></li>
+            <li><NavLink to="tv-shows">TV Shows</NavLink></li>
+            <li><NavLink to="about">About</NavLink></li>
           </ul>
         </div>
-
-        <Button
-          className="p-4"
-          linkTo="profile"
-          children={contextUser ? contextUser.displayName : "Sign In"}
-        />
+        {
+          contextUser ? (
+            <Button linkTo="profile" className="flex items-center">
+              {
+                avatar ? <img src={avatar} alt='avatar' className='avatar-img' /> :
+                  <div className='initial-container'>{initial}</div>
+              }
+            </Button>
+          )
+            :
+            <Button
+              className="p-4"
+              linkTo="profile"
+              children='Sign In'
+            />
+        }
       </div>
     </div>
   );
