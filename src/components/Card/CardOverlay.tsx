58ef2b9fe4174f2c80c4dc18c1f85004cdf45d9b
Jay Oh <jungwon.oh0920@gmail.com>
2023-04-14 15:11:43 -0700

    added movie title on contentIntro page.

--- src/components/Card/CardOverlay.tsx
+++ src/components/Card/CardOverlay.tsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from 'react'
 import Button from '../Button/Button'
 import './card.scss'
 
 function CardOverlay({ data }: any) {
+    const [isMovie, setIsMovie] = useState(false)
+
+    useEffect(() => {
+        if (data.title && data.release_date) {
+            setIsMovie(true)
+        }
+    }, [data.title, data.release_date])
+
     return (
         <div className='card-overlay'>
-            <Button linkTo={`/contents/${data.id}`}>See details</Button>
+            <Button linkTo={`/contents/${isMovie ? 'movie' : 'tv'}/${data.id}`}>See details</Button>
             <Button>Add to favorite</Button>
         </div>
     )
