f11b70afc7814f442fdfb00be9e4bc1678d958f1
Jay Oh <jungwon.oh0920@gmail.com>
2023-05-03 16:13:55 -0700

    added rating for movie content via new prop.

--- src/components/ContentHero/ContentHero.tsx
+++ src/components/ContentHero/ContentHero.tsx
@@ -1,27 +1,31 @@
 import { useState, useEffect } from 'react'
+import { ContentInfoType } from '../../pages/ContentIntro'
 import './content-hero.scss'
 
-const ContentHero = ({ contentData }: any) => {
-    const [backgroundImage, setBackgroundImage] = useState('')
+const ContentHero = (prop: any) => {
+    const [contentInfo, setContentInfo] = useState<ContentInfoType>()
 
     useEffect(() => {
-        contentData && setBackgroundImage(contentData.backdrop_path)
-        console.log('contentData: ', contentData)
-    }, [contentData])
+        if (prop && prop.contentInfo && prop.contentInfo.contentData) {
+            setContentInfo(prop.contentInfo)
+        }
+    }, [prop])
 
     return (
         <div className='content-hero-container'>
-            {/* {contentData ? contentData.title : ''} */}
             <div className='image-background' style={{
-                backgroundImage: `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${backgroundImage})`
+                backgroundImage: `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${contentInfo && contentInfo.contentData.backdrop_path})`
             }}>
             </div>
             <div className='content-container'>
                 <div className='poster-container'>
-                    <img src={`https://image.tmdb.org/t/p/original${contentData && contentData.poster_path}`} alt='post' />
+                    <img src={`https://image.tmdb.org/t/p/original${contentInfo && contentInfo.contentData.poster_path}`} alt='post' />
                 </div>
                 <div className='content-info'>
-                    <h2 style={{ color: 'white' }}>{contentData ? contentData.title : ''}</h2>
+                    <h2>{contentInfo && contentInfo?.contentData.title}</h2>
+                    <div className='facts'>
+                        <span className='rating'>{contentInfo?.rating}</span>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/ContentIntro.tsx
+++ src/pages/ContentIntro.tsx
@@ -3,51 +3,81 @@ import { useLocation } from 'react-router-dom'
 import ContentHero from '../components/ContentHero/ContentHero'
 import "../styles/contentIntro.scss"
 
-enum platformTypes {
+enum PlatformTypes {
     tv,
     movie
+}
+
+interface ReleaseDatesType {
+    iso_3166_1: string,
+    release_dates: [{ certification: string }]
+}
+
+export interface ContentDataType {
+    backdrop_path: string,
+    poster_path: string,
+    title: string
+}
+
+export interface ContentInfoType {
+    contentData: ContentDataType,
+    rating: string
 }
 
 const ContentIntro = () => {
     let location = useLocation()
     const [id, setId] = useState(0)
-    const [platform, setPlatform] = useState<platformTypes>()
-    const [contentData, setContentData] = useState<any>()
+    const [platform, setPlatform] = useState<PlatformTypes>()
+    const [contentInfo, setContentInfo] = useState<ContentInfoType>()
+    const [contentData, setContentData] = useState<ContentDataType>()
+    const [rating, setRating] = useState('')
 
     const key = process.env.REACT_APP_TMDB_API_KEY
 
     useEffect(() => {
         const locationArray = location.pathname.split('/')
         setId(Number(locationArray[3]))
-        setPlatform(locationArray[2] === 'tv' ? platformTypes.tv : platformTypes.movie)
+        setPlatform(locationArray[2] === 'tv' ? PlatformTypes.tv : PlatformTypes.movie)
     }, [location.pathname])
 
-    // useEffect(() => {
-    //     console.log('content: ', contentData)
-    // }, [contentData])
+    useEffect(() => {
+        setContentInfo({
+            contentData: contentData as ContentDataType,
+            rating: rating
+        })
+    }, [contentData, rating])
 
     useEffect(() => {
         const fetchAPI = async () => {
-            if (platform === platformTypes.movie) {
-                await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${key}&language=en-US`).then(res => {
-                    if (res.ok) {
-                        return res.json()
-                    }
-                    throw res
-                }).then(data => setContentData(data)).catch(error => {
-                    console.log('error fetching contentData...')
-                })
+            if (platform === PlatformTypes.movie) {
+                try {
+                    let [movieData, releaseDates] = await Promise.all([
+                        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${key}&language=en-US`).then(res => res.json()),
+                        fetch(`https://api.themoviedb.org/3/movie/${id}/release_dates?api_key=${key}`).then(res => res.json())
+                    ])
+                    setContentData(movieData)
+                    getUSRating(releaseDates.results)
+                } catch (err) {
+                    console.log('err: ', err);
+                }
             }
         }
         fetchAPI()
     }, [platform, id, key])
 
+    const getUSRating = (releaseDates: ReleaseDatesType[]) => {
+        for (let i = 0; i < releaseDates.length; i++) {
+            const curr = releaseDates[i]
+
+            if (curr.iso_3166_1 === 'US') {
+                const rating = curr.release_dates[0].certification
+                setRating(rating)
+            }
+        }
+    }
     return (
         <div className='content-intro-container'>
-            <ContentHero contentData={contentData} />
-            {/* {
-                <p>{contentData ? contentData.title : ''}</p>
-            } */}
+            <ContentHero contentInfo={contentInfo} />
         </div>
     )
 }
