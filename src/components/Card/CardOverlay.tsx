44f8831a1942370d11e475992a1cdba787e11cb7
Jay Oh <jungwon.oh0920@gmail.com>
2023-06-21 14:32:15 -0700

    favorite icons added

--- src/components/Card/CardOverlay.tsx
+++ src/components/Card/CardOverlay.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react'
-import Button from '../Button/Button'
+import Button, { ButtonTypes } from '../Button/Button'
+import favoriteEmptySvg from '../../assets/images/favorite-empty.svg'
+import favoriteFillSvg from '../../assets/images/favorite-fill.svg'
 import './card.scss'
 
 function CardOverlay({ data }: any) {
     const [isMovie, setIsMovie] = useState(false)
+    const [isFavorite, setIsFavorite] = useState(false)
 
     useEffect(() => {
         if (data.title && data.release_date) {
@@ -14,7 +17,11 @@ function CardOverlay({ data }: any) {
     return (
         <div className='card-overlay'>
             <Button linkTo={`/contents/${isMovie ? 'movie' : 'tv'}/${data.id}`}>See details</Button>
-            {/* <Button>Add to favorite</Button> */}
+            <div className='favorite-container'>
+                <Button type={ButtonTypes.noBorder} onClick={() => { setIsFavorite(!isFavorite) }}>
+                    <img src={isFavorite ? favoriteFillSvg : favoriteEmptySvg} alt='favoriteEmptySvg' />
+                </Button>
+            </div>
         </div>
     )
 }
