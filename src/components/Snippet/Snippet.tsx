f48b5b78ee861f96a28d0ec8ac5da82fa27f1111
Jay Oh <jungwon.oh0920@gmail.com>
2023-09-13 14:02:33 -0700

    snippet img hover working

--- src/components/Snippet/Snippet.tsx
+++ src/components/Snippet/Snippet.tsx
@@ -1,20 +1,43 @@
 import { useAppSelector } from '../../hooks'
 import { VideoType } from '../../pages/Home'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import './snippet.scss'
+import { useState } from 'react';
 
 const Snippet = () => {
     const favs: VideoType[] = useAppSelector((state) => state.favorites.movies)
 
-    return (
-        <div>
-            {favs ? favs.map((movie, idx) => {
-                return <div key={idx}>
-                    <img src={`https://image.tmdb.org/t/p/w185/${movie.backdrop_path}`} alt={movie.title} />
-                    <p>{movie.title}</p>
-                    <p>{movie.poster_path}</p>
-                </div>
-            }) : ''}
+    const ImgHoverCover = () => (
+        <div className='img-hover-cover'>
+            <FontAwesomeIcon icon={faCirclePlay} />
         </div>
+    )
+
+    interface singleSnippetProp {
+        movie: VideoType
+    }
+
+    const SingleSnippet = ({ movie }: singleSnippetProp) => {
+        const [isImgHovered, setIsImgHovered] = useState(false)
+
+        return (
+            <div className='snippet' onMouseEnter={() => { setIsImgHovered(true) }} onMouseLeave={() => { setIsImgHovered(false) }}>
+                <div className='snippet-img-container'>
+                    <img className='snippet-img' src={`https://image.tmdb.org/t/p/w185/${movie.backdrop_path}`} alt={movie.title} />
+                    {isImgHovered ? <ImgHoverCover /> : null}
+                </div>
+                <p className={`title ${isImgHovered ? 'hovered' : ''}`}>{movie.title}</p>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            {favs ? favs.map((movie, idx) => {
+                return <div key={idx}><SingleSnippet movie={movie} /></div>
+            }) : ''}
+        </>
     )
 }
 
