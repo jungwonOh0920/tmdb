4fe888c7bfc5ec378d913b6b509e602cffef1fbd
Jay Oh <jungwon.oh0920@gmail.com>
2023-09-21 18:57:19 -0700

    refactoring contentHero and contentInfo components

--- src/components/ContentHero/ContentHero.tsx
+++ src/components/ContentHero/ContentHero.tsx
@@ -1,37 +1,46 @@
 import { useState, useEffect } from 'react'
-import { ContentInfoType } from '../../pages/ContentInfo'
+import { MovieInfoRateType } from '../../pages/ContentInfo'
+import { TVType } from '../../pages/Home'
 import Rate, { SizeType } from '../Rate/Rate'
 import Button from '../Button/Button'
 import './content-hero.scss'
 import Modal from '../Modal/Modal'
 import noPoster from '../../assets/images/noPoster.png'
 
-const ContentHero = (prop: any) => {
-    const [contentInfo, setContentInfo] = useState<ContentInfoType>()
+interface ContentHeroPropType {
+    content: MovieInfoRateType
+}
+
+const ContentHero = ({ content }: ContentHeroPropType) => {
+    const [contentInfo, setContentInfo] = useState<MovieInfoRateType>()
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
-    const [trailerKey, setTrailerKey] = useState<string>('')
+    // const [trailerKey, setTrailerKey] = useState<string>('')
     const key = process.env.REACT_APP_TMDB_API_KEY
 
     useEffect(() => {
-        const fetchAPI = async () => {
-            try {
-                let res = await fetch(`https://api.themoviedb.org/3/movie/${contentInfo?.contentData.id}/videos?api_key=${key}&language=en-US`)
-                const jsonData = await res.json()
-                setTrailerKey(jsonData.results[0].key)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        if (prop && prop.contentInfo && prop.contentInfo.contentData) {
-            setContentInfo(prop.contentInfo)
-            fetchAPI()
-        }
-    }, [prop, contentInfo, key])
+        console.log('content check: ', content);
+    }, [content])
+    // useEffect(() => {
+    //     console.log('contentInfo: ', contentInfo)
+    //     const fetchAPI = async () => {
+    //         try {
+    //             let res = await fetch(`https://api.themoviedb.org/3/movie/${contentInfo?.contentData.id}/videos?api_key=${key}&language=en-US`)
+    //             const jsonData = await res.json()
+    //             setTrailerKey(jsonData.results[0].key)
+    //         } catch (error) {
+    //             console.log(error)
+    //         }
+    //     }
+    //     if (prop && prop.contentInfo && prop.contentInfo.contentData) {
+    //         setContentInfo(prop.contentInfo)
+    //         fetchAPI()
+    //     }
+    // }, [prop, contentInfo, key])
 
-    const getReleaseDate = () => {
-        let date = contentInfo?.contentData.release_date.split('-').join('/')
-        return date
-    }
+    // const getReleaseDate = () => {
+    //     let date = contentInfo?.contentData.release_date.split('-').join('/')
+    //     return date
+    // }
 
     const convertToHour = (mins: number = 0) => {
         const hourNum = Math.floor(mins / 60)
@@ -48,10 +57,8 @@ const ContentHero = (prop: any) => {
 
     return (
         <div className='content-hero-container'>
-            <div className='image-background' style={{
-                backgroundImage: `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${contentInfo && contentInfo.contentData.backdrop_path})`
-            }}>
-            </div>
+            <div className='image-background'
+                style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${content.contentData && content.contentData.backdrop_path})` }}></div>
             {
                 contentInfo &&
                 <section className='content-container'>
@@ -70,7 +77,7 @@ const ContentHero = (prop: any) => {
                                 contentInfo.rating && <span className='rating'>{contentInfo?.rating}</span>
                             }
 
-                            <span className={contentInfo.rating && 'pl-2'}>{getReleaseDate()}</span>
+                            {/* <span className={contentInfo.rating && 'pl-2'}>{getReleaseDate()}</span> */}
                             <span className='genres'>
                                 {
                                     contentInfo.contentData.genres.map(genre => genre.name).join(', ')
@@ -84,7 +91,7 @@ const ContentHero = (prop: any) => {
 
                         <Rate rate={Math.floor(contentInfo.contentData.vote_average || 0)} size={SizeType.medium} />
                         <Button onClick={toggleModal}>Play Trailer</Button>
-                        {
+                        {/* {
                             isModalOpen ?
                                 <>
                                     <Modal modalHeader='Play Trailer' toggleModal={toggleModal}>
@@ -94,7 +101,7 @@ const ContentHero = (prop: any) => {
                                     </Modal>
                                 </>
                                 : ''
-                        }
+                        } */}
                     </section>
                 </section>
             }
